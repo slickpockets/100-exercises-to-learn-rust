52dd5cabd7603415f404b58d7321624f32867bb8
slickpockets <slickpocketsroberts@gmail.com>
2024-05-29 07:47:24 +0800

    03-12 outro half way done

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,67 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        validate_product_name(&product_name);
+        validate_quantity(quantity);
+        validate_unit_price(price);
+
+    }
+    pub fn total(self) -> u32 {
+
+    }
+    pub fn product_name(self) -> String {
+        self.product_name
+    }
+    pub fn quantity(self) -> u32 {
+        self.quantity
+    }
+    pub fn unit_price(self) -> u32 {
+        self.unit_price
+    }
+
+    pub fn set_product_name() {
+
+    }
+    pub fn set_quantity() {
+
+    }
+    pub fn set_unit_price() {
+
+    }
+    
+}
+
+pub fn validate_product_name(name: &String) {
+    if name.is_empty() {
+        panic!("name cant be empty");
+    }
+    if name.len() > 300 {
+        panic!("name cant be longer than 300 bytes");
+    }
+
+}
+
+pub fn validate_quantity(quantity: u32) {
+    if quantity <= 0 {
+        panic!("quantity cant be 0 or less");
+    }
+    quantity
+
+}
+
+pub fn validate_unit_price(price: u32) {
+    if price <= 0 {
+        panic!("price must be more than 0")
+    }
+    price
+
+}
